024f58220b4b26511f6cf42b8ef10c2f1e62c566
Charlie Natoli <charlienatoli@MacBookPro.AN-810-AP-I-AC>
2025-02-10 15:00:02 -0800

    return conversation from backend

--- api_handlers.py
+++ api_handlers.py
@@ -32,9 +32,22 @@ This question should be interesting and intermediate/advanced. For example, some
 {topics}
 
 Don't simply repeat these topics, but use them as a guide for the difficulty level we're looking for. Be creative.
-Ask your questions in {language}. Your question should be friendly, and be 1-2 sentences.
+Always ask your questions in {language}. Your question should be friendly, and be 1-2 sentences.
 Respond with just the question and nothing else: 
 """
+
+CONVERSATION_SYSTEM = """You are a language learning bot who helps intermediate students practice {language}.
+
+Your job is to continue a conversation with your student by asking them another question. 
+Try to expand on whatever they said. Start by responding - for example, "That's interesting because of XYZ" or "That reminds me of XYZ".
+Then pose a follow-up question. 
+If the user is rude, tell them to be respectful. 
+If the user writes something unclear on nonsensical, tell them you don't understand and ask to rephrase. 
+If the user doesn't write in {language}, remind them to use {language}
+
+Always respond to them in {language}. Your response should be 1-2 sentences.  
+"""
+
 
 def start_new_conversation(language="Spanish"):
     topics = "\n".join(random.sample(QUESTION_TYPES, 2))
@@ -51,6 +64,37 @@ def start_new_conversation(language="Spanish"):
     )
 
     return message.content[0].text
+
+
+def _reformat_conversation_for_claude(conversation):
+
+    roles_dict = {
+        "user": "user",
+        "ai":  "assistant"
+    }
+    return [
+        {"role": roles_dict[c["type"]], "content": c["content"]}
+         for c in conversation
+
+    ]
+
+
+def continue_conversation(conversation, language="Spanish"):
+    conversation_for_claude = _reformat_conversation_for_claude(conversation)
+    print("conversation", _reformat_conversation_for_claude(conversation))
+
+    message = CLIENT.messages.create(
+        model="claude-3-5-sonnet-20241022",
+        system=CONVERSATION_SYSTEM.format(language=language),
+        max_tokens=1024,
+        temperature=1,
+        messages=conversation_for_claude
+    )
+
+    return message.content[0].text
+
+
+
 
 
 
--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request, make_response
 from flask_cors import CORS, cross_origin
 
-from api_handlers import start_new_conversation
+from api_handlers import start_new_conversation, continue_conversation
 import time
 
 app = Flask(__name__)
@@ -9,44 +9,37 @@ CORS(app)
 
 @app.route('/new_conversation', methods=['GET'])
 @cross_origin()
-def new_conversation():
-    # language = request.args.get('language')
+def new_conversation_api():
     language= "Spanish"
-    print('REQUEST RECEIVED - SLEEPING')
     new_conversation = start_new_conversation(language)
-    # new_conversation = "BLABA"
-    print('DONE')
-    print(new_conversation)
     response = make_response({'status': 'success', 'new_conversation': new_conversation})
 
-    # Explicitly add CORS headers
-    # response.headers.add('Access-Control-Allow-Origin', '*')  # Or your specific origin
-    # response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
-    # response.headers.add('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
 
     return response
 
 
-@app.route('/add_response',  methods=['GET', 'POST'])
-def add_response():
-  print('REQUEST RECEIVED FOR ADD RESPONSE')
-  time.sleep(1)
-  print('DONE')
-  resp = {
-      "ai_response":"Verdaaaaad??? Dime masssss!!!",
+@app.route('/continue_conversation',  methods=['POST'])
+def continue_conversation_api():
+    conversation = request.json.get('conversation')
+    print('REQUEST RECEIVED FOR CONTINUE CONVO')
+    print("conversation", conversation)
+    response = continue_conversation(conversation)
+    print('DONE')
+    resp = {
+      "ai_response":response,
       "feedback": """
         Style:: could work on XYZ
         Word Choice:: the word \" blah\" does not work here
         Grammar:: you use of the Pluperfect subjunctive text is groovy here
       """ ,
       "key_words": "Agora: now. \n Nao: no. \n San Q: thanks babe"
-  }
+    }
 
-  # Handle POST request data here
-  return jsonify({
+    # Handle POST request data here
+    return jsonify({
       'status': 'success',
       'results': resp
-  })
+    })
 
 
 if __name__ == '__main__':
